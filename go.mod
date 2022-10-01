64c2bbaced34ff83c365ebce58c39b603850c8d6
Andrea Manzini <ilmanzo@gmail.com>
2022-10-01 11:21:32 +0200

    bundle css in assets ; update to go 1.19

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module scannerwebinterface
 
-go 1.17
+go 1.18
--- scan.sh
+++ scan.sh
@@ -4,17 +4,17 @@ TIMESTAMP=$(date +%Y%m%d%H%M%S)
 if [ $# -eq 0 ]; then
   read -p "premi ENTER per iniziare la scansione" pausa
 fi
-#bianco e nero
+#black and white
 scanimage --progress --buffer-size=256 --mode Gray --format=tiff --resolution 150 > /tmp/$TIMESTAMP.tiff
-#colori
+#uncomment for color
 #scanimage --progress --buffer-size=256 --mode color --format=tiff --resolution 300 > /tmp/$TIMESTAMP.tiff
 #
-#scommentare prossima riga per normalizzare colori
+#uncomment next line for color normalization 
 mogrify -normalize -level 10%,90% -sharpen 0x1 /tmp/$TIMESTAMP.tiff
 #####################
-#crea pdf
+#create pdf
 convert /tmp/$TIMESTAMP.tiff -density 300x300 -quality 90 -compress jpeg /home/andrea/scansioni/$TIMESTAMP.pdf
-#scommentare la prossima riga per produrre anche jpg
+#uncomment next line if you want also jpg output
 #convert /tmp/$TIMESTAMP.tiff -density 300x300 -quality 90 -compress jpeg /home/andrea/scansioni/$TIMESTAMP.jpg
 if [ $# -ne 0 ]; then
   echo "Sending mail to $1 ..."
